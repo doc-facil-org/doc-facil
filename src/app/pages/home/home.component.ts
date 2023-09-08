359fe173c60d8cdd1e2ccd3112b54f6284e2e613
PedroRibeiro <pedroribeiro89@gmail.com>
2023-09-07 21:46:28 -0300

    config typography

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,15 +2,21 @@ import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AuthService} from "@auth0/auth0-angular";
 import {MatButtonModule} from "@angular/material/button";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatCardModule} from "@angular/material/card";
+import {MatNativeDateModule} from "@angular/material/core";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-    imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
   public auth =  inject(AuthService);
   logout() { this.auth.logout({ logoutParams: { returnTo: document.location.origin } }); }
+
 }
